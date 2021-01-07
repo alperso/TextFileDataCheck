937db14994a7dfc0ddfb061aced829a5e02d1b2e
alpersahinoner <67909046+alperso@users.noreply.github.com>
2021-01-07 04:00:44 +0300

    Check

---
+++ DosyaKarsilastir/DosyaKarsilastir/Form1.Designer.cs
@@ -0,0 +1,410 @@
+﻿namespace DosyaKarsilastir
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbl_dosyaAdi1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.button7 = new System.Windows.Forms.Button();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(11, 577);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 43);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "1.Text Veri Çek";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(12, 38);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(284, 484);
+            this.listBox1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 525);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Satır Sayisi :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(78, 525);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 6;
+            // 
+            // lbl_dosyaAdi1
+            // 
+            this.lbl_dosyaAdi1.AutoSize = true;
+            this.lbl_dosyaAdi1.Location = new System.Drawing.Point(78, 547);
+            this.lbl_dosyaAdi1.Name = "lbl_dosyaAdi1";
+            this.lbl_dosyaAdi1.Size = new System.Drawing.Size(0, 13);
+            this.lbl_dosyaAdi1.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(8, 547);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Dosya Adı :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(412, 547);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 14;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(342, 547);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(61, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Dosya Adı :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(412, 525);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 13);
+            this.label6.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(342, 525);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(64, 13);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Satır Sayisi :";
+            // 
+            // listBox2
+            // 
+            this.listBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(345, 38);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(284, 484);
+            this.listBox2.TabIndex = 10;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(345, 577);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(129, 43);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "2.Text Veri Çek";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(235, 577);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(61, 43);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Temizle";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(568, 577);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(61, 43);
+            this.button4.TabIndex = 16;
+            this.button4.Text = "Temizle";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button5.Location = new System.Drawing.Point(635, 260);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(77, 83);
+            this.button5.TabIndex = 17;
+            this.button5.Text = "Kontrol Et  Aktar >";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // listBox3
+            // 
+            this.listBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 16;
+            this.listBox3.Location = new System.Drawing.Point(718, 38);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(284, 548);
+            this.listBox3.TabIndex = 18;
+            // 
+            // listBox4
+            // 
+            this.listBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.ItemHeight = 16;
+            this.listBox4.Location = new System.Drawing.Point(1022, 38);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(284, 548);
+            this.listBox4.TabIndex = 19;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(312, 276);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(13, 13);
+            this.label8.TabIndex = 20;
+            this.label8.Text = "+";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(791, 595);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(0, 13);
+            this.label9.TabIndex = 23;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(721, 595);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(64, 13);
+            this.label10.TabIndex = 22;
+            this.label10.Text = "Satır Sayisi :";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(1089, 595);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(0, 13);
+            this.label11.TabIndex = 25;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(1019, 595);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(64, 13);
+            this.label12.TabIndex = 24;
+            this.label12.Text = "Satır Sayisi :";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label14.Location = new System.Drawing.Point(755, 6);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(188, 25);
+            this.label14.TabIndex = 27;
+            this.label14.Text = "BENZER OLANLAR";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label15.Location = new System.Drawing.Point(1068, 6);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(179, 25);
+            this.label15.TabIndex = 28;
+            this.label15.Text = "FARKLI OLANLAR";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(1189, 592);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(117, 28);
+            this.button6.TabIndex = 29;
+            this.button6.Text = "YAZDIR";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label13.Location = new System.Drawing.Point(76, 9);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(84, 25);
+            this.label13.TabIndex = 30;
+            this.label13.Text = "LİSTE 1";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label16.Location = new System.Drawing.Point(437, 6);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(84, 25);
+            this.label16.TabIndex = 31;
+            this.label16.Text = "LİSTE 2";
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button7.Location = new System.Drawing.Point(635, 349);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(77, 83);
+            this.button7.TabIndex = 32;
+            this.button7.Text = "Yeni Kontrol";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1324, 632);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.listBox4);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.lbl_dosyaAdi1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Dosya Karşılaştır";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbl_dosyaAdi1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.ListBox listBox4;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+    }
+}
+
---
+++ DosyaKarsilastir/DosyaKarsilastir/Form1.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace DosyaKarsilastir
+{
+    public partial class Form1 : Form
+    {
+        OpenFileDialog openFile = new OpenFileDialog();
+        string line = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<string> liste_A = new List<string>();
+        List<string> liste_B = new List<string>();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            openFile.Filter = "Text Files (.txt)| *.txt";
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button5.Enabled = false;
+            button6.Enabled = false;
+            button7.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                liste_A.Clear();
+                listBox1.Items.Clear();
+                if (openFile.ShowDialog() == DialogResult.OK)
+                {
+                    StreamReader sr = new StreamReader(openFile.FileName);
+                    lbl_dosyaAdi1.Text = openFile.FileName;
+                    while (line != null)
+                    {
+                        line = sr.ReadLine();
+                        if (line != null)
+                        {
+
+                            listBox1.Items.Add(line.Trim());
+                            liste_A.Add(line.Trim());
+                        }
+                    }
+                    line = "";
+                    sr.Dispose();
+                    sr.Close();
+                }
+                label2.Text = listBox1.Items.Count.ToString();
+                button3.Enabled = true;
+                button4.Enabled = true;
+            }
+            catch
+            {
+                MessageBox.Show("Listelenmedi.", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Clear();
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader sr = new StreamReader(openFile.FileName);
+                label3.Text = openFile.FileName;
+                while (line != null)
+                {
+                    line = sr.ReadLine();
+                    if (line != null)
+                    {
+
+                        listBox2.Items.Add(line.Trim());
+                        liste_A.Add(line.Trim());
+                    }
+                }
+                line = "";
+                sr.Dispose();
+                sr.Close();
+            }
+            label6.Text = listBox2.Items.Count.ToString();
+            button5.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            lbl_dosyaAdi1.Text = "";
+            label2.Text = "";
+            listBox1.Items.Clear();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label3.Text = "";
+            label6.Text = "";
+            listBox2.Items.Clear();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            listBox3.Items.Clear();
+            listBox4.DataSource = null;
+            liste_B.Clear();
+            
+            ////Aynı Olanlar
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                for (int j = 0; j < listBox2.Items.Count; j++)
+                {
+                    if (listBox1.Items[i].ToString() == listBox2.Items[j].ToString())
+                    {
+                        listBox3.Items.Add(listBox1.Items[i].ToString());
+                        // listBox3.Items.Add(listBox1.Items[i].ToString() + " " + i);
+                        //listBox2.Items.Remove(j);
+                        liste_B.Add(listBox1.Items[i].ToString());
+                    }
+
+                }
+                //listBox1.Items.Remove(i);
+            }
+
+            label9.Text = listBox3.Items.Count.ToString();//Aynı Olanlar
+                                                          //listBox3.Items.Add(listBox1.Items.Count + " *** " + listBox2.Items.Count + " *** " + listBox3.Items.Count);
+
+            //for (int i = 0; i < listBox3.Items.Count; i++)
+            //{
+               
+            //}
+         
+
+
+            
+
+
+
+
+            listBox4.DataSource = liste_A.Except(liste_B).ToList();//A da olup B de olmayan
+            //listBox4.DataSource= liste_B.Except(liste_A).ToList();//B de olup A da olmayan
+            label11.Text = listBox4.Items.Count.ToString();//Aynı Olanlar
+            // listBox4.DataSource = liste_A.Except(liste_B).ToList();
+            //listBox4.DataSource = listBox1.Items.OfType<string>().ToArray().Except(listBox2.Items.OfType<string>().ToArray()).Union(listBox2.Items.OfType<string>().ToArray().Except(listBox1.Items.OfType<string>().ToArray())).ToArray();
+
+            button2.Enabled = false;
+            button1.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button5.Enabled = false;
+            button6.Enabled = true;
+            button7.Enabled = true;
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Clear();
+            listBox1.Items.Clear();
+            listBox3.Items.Clear();
+            listBox4.DataSource=null;
+            liste_B.Clear();            
+            button1.Enabled = true;
+            button2.Enabled = true;
+            label9.Text = "";
+            label11.Text = "";
+            lbl_dosyaAdi1.Text = "";
+            label2.Text = "";
+            label3.Text = "";
+            label6.Text = "";
+            button6.Enabled = false;
+            button7.Enabled = false;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (openFile.ShowDialog() == DialogResult.OK)
+                {
+                    StreamReader sr = new StreamReader(openFile.FileName);
+                    //label3.Text = openFile.FileName;
+                    sr.Dispose();
+                    sr.Close();
+                    string dosya_yolu = openFile.FileName;
+                    using (TextWriter TW = new StreamWriter(dosya_yolu))
+                    {
+                        foreach (string itemText in listBox3.Items)
+                        {
+                            TW.WriteLine(itemText);
+                        }
+                        foreach (string itemText in listBox4.Items)
+                        {
+                            TW.WriteLine(itemText);
+                        }
+                    }
+                    Process.Start(dosya_yolu);
+                    //string[] kayit_Dizisi = new string[listBox3.Items.Count + listBox4.Items.Count];
+                    //listBox3.Items.CopyTo(kayit_Dizisi, 0);
+                    //listBox4.Items.CopyTo(kayit_Dizisi, 0);
+                    //System.IO.File.WriteAllLines(dosya_yolu, kayit_Dizisi);
+                    //MessageBox.Show("Başarılı");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Hata Bilgisi");
+            }
+            
+
+               
+
+            }
+           
+        }
+    }
+
+    
